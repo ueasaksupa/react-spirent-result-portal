e452d741faf48c59e53bab4e9884bc6b28cfb808
Nuttawut <nueasaks@cisco.com>
2023-05-23 01:42:03 +0700

    fix env

--- frontend/src/components/api/backend.js
+++ frontend/src/components/api/backend.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const API_ADDRESS = process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL.replace(/\/+$/, "")}`
+    : `http://localhost:5001`;
+
 export default axios.create({
-    baseURL: "http://127.0.0.1:5001/api",
+    baseURL: `${API_ADDRESS.replace(/\/+$/, "")}/api`,
 });
